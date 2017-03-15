26d5212c76e6475f0d26ed2dd605df0050e89f4c
calexil <calexil@yahoo.com>
2017-03-14 20:55:13 -0400

    gamepad detected!

--- python/fightstick.py
+++ python/fightstick.py
@@ -2,17 +2,26 @@
 # -*- coding: utf-8 -*-
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-import os, sys, time, pygame
+from gi.repository import Gtk, Gdk
+import os, sys, time
+import inputs
+from inputs import devices
+print(devices.gamepads)
+from inputs import get_gamepad
 file_name = ("images/fightstickclear.png")
+up = ("images/up.png")
 
-pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-print (joysticks)
+
+events = get_gamepad()
+for event in events:
+    event.code=('ABS_HAT0Y', -1)
+    self.overlay = Gtk.Overlay()
+    self.add(self.overlay)
+    self.image.set_from_file(up)
+
 class GUI:
 
 	def __init__(self):
-
             self.window = Gtk.Window()
             self.image = Gtk.Image()
             self.image.set_from_file(file_name)
@@ -21,7 +30,7 @@ class GUI:
             self.window.add(self.image)
             self.window.show_all()
             self.window.connect_after('destroy', self.destroy)
-
+            
 	def destroy(window, self):
 		Gtk.main_quit()
 
