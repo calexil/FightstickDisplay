991b9bdcd813725c93feba5ea358d4baa8c56be0
calexil <calexil@yahoo.com>
2017-04-11 19:36:27 -0400

    mapping testing

--- fightstick.py
+++ fightstick.py
@@ -175,40 +175,7 @@ def update_deadzone(slider):
 
 def remap_buttons(button):
     # in process TODO
-    try:
-        to_unicode = unicode
-    except NameError:
-        to_unicode = str
-
-# Define data
-data = {{
-  "background": [0, 0],
-  "stick":      [118, 155],
-  "select":     [0, 0],
-  "start":      [0, 0],
-  "lp":         [257, 85],
-  "mp":         [337, 115],
-  "hk":         [422, 114],
-  "rb":         [508, 111],
-  "lk":         [276, 175],
-  "mk":         [355, 205],
-  "lb":         [441, 204],
-  "hp":         [528, 201]
-}}
-
-# Write JSON file
-with io.open('layout.json', 'w', encoding='utf8') as outfile:
-    str_ = json.dumps(data,
-                      indent=4, sort_keys=True,
-                      separators=(',', ': '), ensure_ascii=False)
-    outfile.write(to_unicode(str_))
-
-# Read JSON file
-with open('layout.json') as data_file:
-    data_loaded = json.load(data_file)
-
-print(data == data_loaded)
-
+    pass
 
 
 config_layout = VLayout(children=[
