ef5f2dcd068331ea4082884377e34e36e82015a2
calexil <calexil@yahoo.com>
2017-05-14 22:12:02 -0400

    scalable

--- fightstick.py
+++ fightstick.py
@@ -1,5 +1,5 @@
 import pyglet
-#from pyglet.gl import *
+from pyglet.gl import *
 from configparser import ConfigParser
 
 #######################################################
@@ -15,17 +15,17 @@ config = ConfigParser()
 FIGHTSTICK_PLUGGED = False
 
 
-#@window.event
-#def on_resize(width, height):
-#    glViewport(0, 0, width, height)
-#    glMatrixMode(GL_PROJECTION)
-#    glLoadIdentity()
-#    glOrtho(0, width, 0, height, -1, 1)
-#    glMatrixMode(GL_MODELVIEW)
-#    glLoadIdentity()
-#    scale_x = width / 640
-#    scale_y = height / 391
-#    glScalef(scale_x, scale_y, 1.0)
+@window.event
+def on_resize(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, width, 0, height, -1, 1)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    scale_x = width / 640.0
+    scale_y = height / 391.0
+    glScalef(scale_x, scale_y, 1.0)
 
 
 _layout = {
