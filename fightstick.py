d0280320411e9e9f26cc1c9c81a9c2ec85f8978a
Benjamin Moran <bmoran@blueshift.co.jp>
2017-05-16 11:20:29 +0900

    Changed star gl import to explicit imports.

--- fightstick.py
+++ fightstick.py
@@ -1,5 +1,6 @@
 import pyglet
-from pyglet.gl import *
+from pyglet.gl import glViewport, glMatrixMode, glOrtho, glLoadIdentity, glScalef
+from pyglet.gl import GL_PROJECTION, GL_MODELVIEW
 from configparser import ConfigParser
 
 #######################################################
