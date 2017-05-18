6575461c235be1a817a384cfcc93f7c37a0cfaba
calexil <calexil@yahoo.com>
2017-05-17 22:26:29 -0400

    cleanup

--- fightstick.py
+++ fightstick.py
@@ -16,12 +16,12 @@ FIGHTSTICK_PLUGGED = False
 
 @window.event
 def on_resize(width, height):
-    aspect_ratio = 640.0/390.0
-    target_width = int(height * aspect_ratio)
-    target_height = int(width / aspect_ratio)
-    if window.width != target_width and window.height != target_height:
-        window.set_maximum_size(target_width+100, target_height+100)
-        window.set_size(window.width, target_height)    
+#    aspect_ratio = 640.0/390.0
+#    target_width = int(height * aspect_ratio)
+#    target_height = int(width / aspect_ratio)
+#    if window.width != target_width and window.height != target_height:
+#        window.set_maximum_size(target_width+100, target_height+100)
+#        window.set_size(window.width, target_height)    
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
