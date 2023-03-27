1df4c891dcded42438568d635b9cc1f85b689731
calexil <admin@calexil.com>
2023-03-27 01:50:53 -0400

    fixing HB version, fixing linting, fixing typos

--- fightstick.py
+++ fightstick.py
@@ -130,32 +130,6 @@ class TryAgainScene:
         self.missing_img.blit(0, 0)
 
 
-# # Deadzone Interface, ?maybe TODO This doesn't work.
-# class DeadzoneScene:
-#     def __init__(self, window_instance):
-#         self.window = window_instance
-#         self.deadzone_img = pyglet.resource.image("deadzone.png")
-#
-#         @self.window.event
-#         def on_button_press(controller, button):
-#             assert _debug_print(f"Pressed Button: {button}")
-#             pressed_button = button_mapping.get(button, None)
-#             if pressed_button == 'guide':
-#                 if config_window.parent is not None:
-#                     self.frame.remove(config_window)
-#                 else:
-#                     self.frame.add(config_window)
-#
-#         def update_trigger_point(slider):
-#             self.triggerpoint = slider.value
-#             deadzone_label = self.frame.get_element_by_name("triggerpoint")
-#             deadzone_label.text = "Analog Trigger Point: {}".format(round(slider.value, 2))
-#
-#         def on_draw():
-#             self.window.clear()
-#             self.deadzone_img.blit(0, 0)
-
-
 class MainScene:
     """ The main scene, with all fightstick events wired up."""
 
--- fightstick_hb.py
+++ fightstick_hb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import urllib.request
 
 from weakref import proxy
 from configparser import ConfigParser
@@ -8,7 +9,6 @@ import pyglet
 
 from pyglet.util import debug_print
 from pyglet.math import Mat4, Vec3
-
 
 # Set up the debugging flag calls.
 _debug_flag = len(sys.argv) > 1 and sys.argv[1] in ('-D', '-d', '--debug')
@@ -28,15 +28,6 @@ _debug_print("Main window created")
 # Use configParser to set a static controller status of unplugged.
 config = ConfigParser()
 
-
-if os.path.exists("gamecontrollerdb.txt"):
-    try:
-        pyglet.input.controller.add_mappings_from_file("gamecontrollerdb.txt")
-        print("Added additional controller mappings from 'gamecontrollerdb.txt'")
-    except Exception as e:
-        print(f"Failed to parse 'gamecontrollerdb.txt'. Please open an issue on GitHub. \n --> {e}")
-
-
 # Parse and add additional SDL controller mappings.
 url = "https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt"
 try:
@@ -51,12 +42,13 @@ except Exception:
             print(f"Failed to parse 'gamecontrollerdb.txt'. Please open an issue on GitHub. \n --> {e}")
 
 
+# Math for scaling the window when resized.
 @window.event
 def on_resize(width, height):
     projection_matrix = Mat4.orthogonal_projection(0, width, 0, height, 0, 1)
     scale_x = width / 640.0
     scale_y = height / 390.0
-    window.projection = projection_matrix.scale(scale_x, scale_y, 1)
+    window.projection = projection_matrix.scale(Vec3(scale_x, scale_y, 1))
     window.viewport = 0, 0, width, height
     return pyglet.event.EVENT_HANDLED
 
@@ -66,6 +58,7 @@ _layout = {
     "background": (0, 0),
     "select": (50, 318),
     "start": (50, 318),
+    "guide": (50, 318),
     "up": (237, 10),
     "down": (133, 217),
     "left": (47, 217),
@@ -85,6 +78,7 @@ _images = {
     'background': 'backgroundHB.png',
     'select': 'select.png',
     'start': 'start.png',
+    'guide': 'guide.png',
     'up': 'buttonhblg.png',
     'down': 'buttonhb.png',
     'left': 'buttonhb.png',
@@ -143,12 +137,14 @@ class TryAgainScene:
 
 
 class MainScene:
-    # The Main Scene, with all fightstick events rigged.
-    def __init__(self, window_instance, fightstick):
-        self.window = window_instance
+    """ The main scene, with all fightstick events wired up."""
+
+    def __init__(self, window_instance):
+        self.window = proxy(window_instance)
         self.batch = pyglet.graphics.Batch()
-        self.fightstick = fightstick
-        self.fightstick.open()
+        # Ordered groups to handle draw order of the sprites.
+        self.bg = pyglet.graphics.Group(0)
+        self.fg = pyglet.graphics.Group(1)
         # Ordered groups to handle draw order of the sprites.
         self.bg = pyglet.graphics.Group(0)
         self.fg = pyglet.graphics.Group(1)
@@ -156,6 +152,7 @@ class MainScene:
         self.background = _make_sprite('background', self.batch, self.bg)
         self.select_spr = _make_sprite('select', self.batch, self.fg, False)
         self.start_spr = _make_sprite('start', self.batch, self.fg, False)
+        self.guide_spr = _make_sprite('guide', self.batch, self.fg, False)
         self.up_spr = _make_sprite('up', self.batch, self.fg, False)
         self.down_spr = _make_sprite('down', self.batch, self.fg, False)
         self.left_spr = _make_sprite('left', self.batch, self.fg, False)
@@ -172,27 +169,27 @@ class MainScene:
         self.deadzone = 0.2
 
         # Mapping and press/axis/abs event section below.
-        self.button_mapping = {"back": self.select_spr, "start": self.start_spr,
-                          "up": self.up_spr, "down": self.down_spr, "left": self.left_spr, "right": self.right_spr,
-                          "a": self.a_spr, "b": self.b_spr, "x": self.x_spr, "y": self.y_spr,
-                          "rightshoulder": self.rb_spr, "leftshoulder": self.lb_spr,
-                          "righttrigger": self.rt_spr, "lefttrigger": self.lt_spr}
+        self.button_mapping = {"back": self.select_spr, "start": self.start_spr, "guide": self.guide_spr,
+                               "up": self.up_spr,    "down":self.down_spr, "left": self.left_spr, 
+                               "right": self.right_spr, "a": self.a_spr, "b": self.b_spr, "x": self.x_spr,
+                               "y": self.y_spr, "rightshoulder": self.rb_spr, "leftshoulder": self.lb_spr,
+                               "righttrigger": self.rt_spr, "lefttrigger": self.lt_spr}
 
     # Event to show a button when pressed.
-    def on_button_press(controller, button):
+    def on_button_press(self, controller, button):
         assert _debug_print(f"Pressed Button: {button}")
-        pressed_button = button_mapping.get(button, None)
+        pressed_button = self.button_mapping.get(button, None)
         if pressed_button:
             pressed_button.visible = True
 
     # Event to hide the sprite when the button is released.
-    def on_button_release(controller, button):
-        pressed_button = button_mapping.get(button, None)
+    def on_button_release(self, controller, button):
+        pressed_button = self.button_mapping.get(button, None)
         if pressed_button:
             pressed_button.visible = False
 
     # Have the dpad hats alert the main window to draw the sprites.
-    def on_dpad_motion(controller, dpleft, dpright, dpup, dpdown):
+    def on_dpad_motion(self, controller, dpleft, dpright, dpup, dpdown):
         assert _debug_print(f"Dpad  Left:{dpleft}, Right:{dpright}, Up:{dpup}, Down:{dpdown}")
         # this is dumb, refactor this..
         if dpup is True:
@@ -214,21 +211,21 @@ class MainScene:
             
 
     # Math to draw trigger inputs or hide them.
-    def on_trigger_motion(controller, trigger, value):
+    def on_trigger_motion(self, controller, trigger, value):
         assert _debug_print(f"Pulled Trigger: {trigger}")
         if trigger == "lefttrigger":
             if value > self.triggerpoint:
                 self.lt_spr.visible = True
-            elif value < -self.triggerpoint:
+            elif value < self.triggerpoint:
                 self.lt_spr.visible = False
         if trigger == "righttrigger":
             if value > self.triggerpoint:
                 self.rt_spr.visible = True
-            elif value < -self.triggerpoint:
+            elif value < self.triggerpoint:
                 self.rt_spr.visible = False
 
     # Window event to draw everything when necessary.
-    def on_draw():
+    def on_draw(self):
         self.window.clear()
         self.batch.draw()
 
